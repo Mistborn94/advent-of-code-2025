675ca48c71333d5fd704c47d1818e561ba08790a
Renette Ros <supergirl0894+git@gmail.com>
2025-12-04 07:36:57 +0200

    Simplify Day 4 part 2

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -3,6 +3,7 @@ package day4
 import helper.Debug
 import helper.point.base.points
 import helper.point.base.*
+import helper.removeFirst
 
 fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
     val grid = text.lines()
@@ -16,16 +17,17 @@ fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
 
 fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
     val grid = text.lines()
-    val points = grid.points().filterTo(mutableSetOf()) { grid[it] == '@'}
+    val toVisit = grid.points().filterTo(mutableSetOf()) { grid[it] == '@'}
     val removed = mutableSetOf<Point>()
 
     do {
-        val newRemoved = points.filterTo(mutableSetOf()) {
-            grid[it] == '@' && (it.neighbours() + it.diagonalNeighbours()).count { n -> n in grid && n !in removed && grid[n] == '@' } < 4
+        val next = toVisit.removeFirst()
+        val pointNeighbours = (next.neighbours() + next.diagonalNeighbours()).filter { n -> n in grid && n !in removed && grid[n] == '@' }
+        if (pointNeighbours.size < 4) {
+            removed.add(next)
+            toVisit.addAll(pointNeighbours)
         }
-        removed.addAll(newRemoved)
-        points.removeAll(newRemoved)
-    } while (newRemoved.isNotEmpty())
+    } while (toVisit.isNotEmpty())
 
     return removed.size
 }
